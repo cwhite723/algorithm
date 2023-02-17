d27c1f89113d2028a521852292501b70e4496f31
Hayan <33473174+cwhite723@users.noreply.github.com>
2023-02-17 17:57:17 +0900

    [Bronze IV] Title: 주사위 세개, Time: 120 ms, Memory: 14164 KB -BaekjoonHub

--- 백준/Bronze/2480. 주사위 세개/주사위 세개.java
+++ 백준/Bronze/2480. 주사위 세개/주사위 세개.java
@@ -1,30 +1,35 @@
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
 import java.util.StringTokenizer;
+import java.util.TreeSet;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        
-        int A = Integer.parseInt(st.nextToken());
-        int B = Integer.parseInt(st.nextToken());
-        int C = Integer.parseInt(st.nextToken());
-        int Max = 0;
+        ArrayList<Integer> arr = new ArrayList<>();
+        TreeSet<Integer> set = new TreeSet<>();
+
         int price = 0;
-        if(A==B) {
-            if(A==C)
-                price = 10000+(A*1000);
+
+        for(int i=0; i<3; i++) {
+            arr.add(Integer.parseInt(st.nextToken()));
+            set.add(arr.get(i));
+        }
+
+        if(set.size()==1)
+            price = 10000 + (arr.get(0)*1000);
+        else if(set.size()==2) {
+            if(arr.get(0)==arr.get(1))
+                price = 1000 + (arr.get(0)*100);
             else
-                price = 1000+(A*100);
+                price = 1000 + (arr.get(2)*100);
         }
-        else if(B==C)
-            price = 1000+(B*100);
-        else if(A==C)
-            price = 1000+(A*100);
-        else {
-            Max = Math.max(Math.max(A,B),C);
-            price = 100*Max;
-        }
+        else
+            price = 100 * set.last();
+
         System.out.println(price);
     }
 }
