82c5523efd3559bb291082e0ab7db30222d9c87f
Hayan <33473174+cwhite723@users.noreply.github.com>
2023-03-01 20:00:28 +0900

    [Bronze V] Title: N 찍기, Time: 212 ms, Memory: 18068 KB -BaekjoonHub

--- 백준/Bronze/2741. N 찍기/N 찍기.java
+++ 백준/Bronze/2741. N 찍기/N 찍기.java
@@ -5,9 +5,12 @@ import java.io.InputStreamReader;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        
         int N = Integer.parseInt(br.readLine());
         for(int i=1; i<=N; i++) {
-            System.out.println(i);
+            sb.append(i).append("\n");
         }
+        System.out.println(sb.toString());
     }
 }
