f00077122ee59da1b1ec978d6f2ac78c9a437294
Hayan <33473174+cwhite723@users.noreply.github.com>
2023-03-14 16:59:42 +0900

    [level 0] Title: 다음에 올 숫자, Time: 0.01 ms, Memory: 75.8 MB -BaekjoonHub

--- 프로그래머스/lv0/120924. 다음에 올 숫자/다음에 올 숫자.java
+++ 프로그래머스/lv0/120924. 다음에 올 숫자/다음에 올 숫자.java
@@ -1,19 +1,9 @@
 class Solution {
     public int solution(int[] common) {
         int answer = 0;
-        int difference1, difference2, ratio;
-
-        difference1 = common[1]-common[0];
-        difference2 = common[2]-common[1];
         
-        if(difference1 == difference2)
-            answer = common[common.length-1] + difference1;
-        else {
-            ratio = common[1]/common[0];
-            answer = common[common.length-1] * ratio;
-        }
-            
-        
+        if(common[2]-common[1] == common[1]-common[0]) answer = common[common.length-1] + common[1]-common[0];
+        else answer = common[common.length-1] * common[common.length-2]/common[common.length-3];
         return answer;
     }
 }
