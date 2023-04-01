11c7f9bd5c84ca71d746b37ff055e8e8537a9972
Hayan <33473174+cwhite723@users.noreply.github.com>
2023-04-02 03:12:27 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 7.68 ms, Memory: 56.2 MB -BaekjoonHub

--- 프로그래머스/lv2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
+++ 프로그래머스/lv2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
@@ -1,30 +1,41 @@
 import java.util.*;
 class Solution {
-    public int solution(int[][] maps) {
-        int answer = 0;
-        int n = maps.length;
-        int m = maps[0].length;
+    int[][] maps;
+    int answer;
+    int[][] dist;
+    
+    void bfs(int[] start) {
         Queue<int[]> qu = new LinkedList<>();
-        int[][] visited = new int[n][m];
-        
-        int[] xPoint = {0, -1, 0, 1};
-        int[] yPoint = {1, 0, -1, 0};
+        qu.add(start);
+        int[] dx = {0, -1, 0, 1};
+        int[] dy = {1, 0, -1, 0};
 
-        qu.add(new int[]{0, 0});
-        visited[0][0] = 1;
         while(!qu.isEmpty()) {
-            int[] start = qu.poll();
+            int[] current = qu.poll();
+            int x = current[0];
+            int y = current[1];
             for(int i=0; i<4; i++) {
-                int dx = start[0]+xPoint[i];
-                int dy = start[1]+yPoint[i];
-                if(dx>=0&&dy>=0&&dx<n&&dy<m) {
-                    if(visited[dx][dy]==0&&maps[dx][dy]==1) {
-                        visited[dx][dy] = visited[start[0]][start[1]]+1;
-                        qu.add(new int[]{dx, dy});
+                int nextX = x + dx[i];
+                int nextY = y + dy[i];
+                if(nextX>=0&&nextY>=0&&nextX<maps.length&&nextY<maps[0].length) {
+                    if(maps[nextX][nextY]==1&&dist[nextX][nextY]==0) {
+                        dist[nextX][nextY] = dist[x][y] + 1;
+                        qu.add(new int[]{nextX, nextY});
                     }
                 }
             }
         }
-        return visited[n-1][m-1]==0 ? -1 : visited[n-1][m-1];
+        if(dist[maps.length-1][maps[0].length-1]==0) answer = -1;
+        else answer = dist[maps.length-1][maps[0].length-1]+1;
+    }
+    
+    public int solution(int[][] maps) {
+        this.maps = maps;
+        this.answer = 0;
+        this.dist = new int[maps.length][maps[0].length];
+        
+        bfs(new int[]{0, 0});
+        System.out.println(answer);
+        return answer;
     }
 }
