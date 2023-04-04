df6372cbd8a43ba320a07bdec3ec6923b923eb9b
Hayan <33473174+cwhite723@users.noreply.github.com>
2023-04-04 16:54:33 +0900

    [Silver I] Title: IOIOI, Time: 136 ms, Memory: 14264 KB -BaekjoonHub

--- 백준/Silver/5525. IOIOI/IOIOI.java
+++ 백준/Silver/5525. IOIOI/IOIOI.java
@@ -6,30 +6,24 @@ import java.util.Queue;
 
 public class Main {
     private static final BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    private static final String ADD_WORD = "OI";
+
     public static void main(String[] args) throws IOException {
         int findSize = Integer.parseInt(br.readLine());
         int wordSize = Integer.parseInt(br.readLine());
-        String word = br.readLine();
+        char[] word = br.readLine().toCharArray();
 
         int result = 0;
+        int wordLen = 3 + (2 * (findSize-1));
 
-        StringBuilder findWord = new StringBuilder();
-        findWord.append("IOI");
-
-        for (int i=1; i<findSize; i++) {
-            findWord.append(ADD_WORD);
-        }
-
-        int endIndex = findWord.length();
-
-        for (int i=0; i<word.length()-findWord.length()+1; i++) {
-            if(word.charAt(i)=='I') {
-                if(word.substring(i, i+findWord.length()).equals(findWord.toString())) result++;
+        loop: for (int i=0; i<wordSize-wordLen+1; i++) {
+            if(word[i]=='I') {
+                for(int k=i+1; k<i+wordLen; k++) {
+                    if(word[k]==word[k-1]) continue loop;
+                }
+                result++;
             }
         }
 
         System.out.println(result);
-
     }
 }
