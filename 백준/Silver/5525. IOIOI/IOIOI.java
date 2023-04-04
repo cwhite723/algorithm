cfa1589850fe1f87c4d5a0ff6ec7b3e7fa08598f
Hayan <33473174+cwhite723@users.noreply.github.com>
2023-04-04 16:16:44 +0900

    [Silver I] Title: IOIOI, Time: 140 ms, Memory: 18920 KB -BaekjoonHub

---
+++ 백준/Silver/5525. IOIOI/IOIOI.java
@@ -0,0 +1,33 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class Main {
+    private static final BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    private static final String ADD_WORD = "OI";
+    public static void main(String[] args) throws IOException {
+        int findSize = Integer.parseInt(br.readLine());
+        int wordSize = Integer.parseInt(br.readLine());
+        String word = br.readLine();
+
+        int result = 0;
+
+        StringBuilder findWord = new StringBuilder();
+        findWord.append("IOI");
+
+        for (int i=1; i<findSize; i++) {
+            findWord.append(ADD_WORD);
+        }
+
+        int endIndex = findWord.length();
+
+        for (int i=0; i<word.length()-findWord.length()+1; i++) {
+            if(word.substring(i, endIndex++).equals(findWord.toString())) result++;
+        }
+
+        System.out.println(result);
+
+    }
+}
