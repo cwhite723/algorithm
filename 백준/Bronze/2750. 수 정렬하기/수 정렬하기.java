b0483fab4a554e419cff1924c72db0437a3480b4
Hayan <33473174+cwhite723@users.noreply.github.com>
2023-04-21 00:38:20 +0900

    [Bronze II] Title: 수 정렬하기, Time: 180 ms, Memory: 18756 KB -BaekjoonHub

--- 백준/Bronze/2750. 수 정렬하기/수 정렬하기.java
+++ 백준/Bronze/2750. 수 정렬하기/수 정렬하기.java
@@ -1,24 +1,58 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Iterator;
-import java.util.TreeSet;
+import java.util.*;
 
 public class Main {
+    static int[] sorted;
+    
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int N = Integer.parseInt(br.readLine());
-        ArrayList<Integer> array = new ArrayList<>();
+        int n = Integer.parseInt(br.readLine());
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(br.readLine());
+        }
+        mergeSort(arr, 0, arr.length-1);
 
-        for(int i=0; i<N; i++) {
-            array.add(Integer.parseInt(br.readLine()));
+        for(int i = 0; i < arr.length; i++) {
+            System.out.println(arr[i]);
+        }
+    }
+
+    static void mergeSort(int[] arr, int start, int end) {
+        if (start < end) {
+            int mid = (start + end) / 2;
+            mergeSort(arr, start, mid);
+            mergeSort(arr, mid + 1, end);
+            merge(arr, start, mid, end);
+        }
+    }
+
+    static void merge(int[] arr, int start, int mid, int end) {
+        sorted = new int[arr.length];
+        int leftIdx = start;
+        int midIdx = mid + 1;
+        int index = start;
+        int i;
+
+        while (leftIdx <= mid && midIdx <= end) {
+            if (arr[leftIdx] <= arr[midIdx]) sorted[index++] = arr[leftIdx++];
+            else sorted[index++] = arr[midIdx++];
         }
 
-        TreeSet<Integer> newArray = new TreeSet<>(array);
-        Iterator iter = newArray.iterator();
-        while(iter.hasNext()) {
-            System.out.println(iter.next());
+        if (leftIdx <= mid) {
+            for (i = leftIdx; i <= mid; i++) {
+                sorted[index++] = arr[i];
+            }
+        } else {
+            for (i = midIdx; i <= end; i++) {
+                sorted[index++] = arr[i];
+            }
+        }
+
+        for (i = start; i<= end; i++) {
+            arr[i] = sorted[i];
         }
     }
 }
