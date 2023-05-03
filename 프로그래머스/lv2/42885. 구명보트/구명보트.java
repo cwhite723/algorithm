b8bf926f7c5b8314745040e31f87a48f97fbc3d3
Hayan <33473174+cwhite723@users.noreply.github.com>
2023-05-04 01:04:21 +0900

    [level 2] Title: 구명보트, Time: 11.61 ms, Memory: 53.2 MB -BaekjoonHub

--- 프로그래머스/lv2/42885. 구명보트/구명보트.java
+++ 프로그래머스/lv2/42885. 구명보트/구명보트.java
@@ -3,12 +3,20 @@ class Solution {
     public int solution(int[] people, int limit) {
         int answer = 0;
         Arrays.sort(people);
-        int i=people.length-1;
-        int k = 0;
-        while(k<=i) {
-            if(people[i]+people[k]<=limit) k++;
-            i--;
+        
+        int leftIdx = 0;
+        int rightIdx = people.length - 1;
+        
+        while (leftIdx < rightIdx) {
+            if (people[leftIdx] + people[rightIdx] <= limit) {
+                rightIdx--;
+                leftIdx++;
+            } else {
+                rightIdx--;
+            }
             answer++;
+            
+            if (leftIdx == rightIdx) answer++;
         }
         return answer;
     }
