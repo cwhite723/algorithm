e32d088479c8cb14864d00439011ad56b9b562f1
Hayan <33473174+cwhite723@users.noreply.github.com>
2023-06-15 00:31:18 +0900

    [Gold V] Title: 선수과목 (Prerequisite), Time: 716 ms, Memory: 128592 KB -BaekjoonHub

---
+++ 백준/Gold/14567. 선수과목 （Prerequisite）/선수과목 （Prerequisite）.java
@@ -0,0 +1,70 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+    private static int cntSubject;
+    private static List<List<Integer>> graph;
+    private static int[] degree;
+    private static int[] result;
+
+    public static void main(String[] args) throws IOException {
+        input();
+        topologicalSort();
+        output();
+    }
+
+    private static void topologicalSort() {
+        result = new int[cntSubject + 1];
+        Queue<Integer> queue = new LinkedList<>();
+
+        for (int i = 1; i <= cntSubject; i++) {
+            if (degree[i] == 0) {
+                queue.offer(i);
+                result[i] = 1;
+            }
+        }
+
+        while (!queue.isEmpty()) {
+            int current = queue.poll();
+
+            for (int i : graph.get(current)) {
+                degree[i]--;
+                if (degree[i] == 0) {
+                    queue.offer(i);
+                    result[i] = result[current] + 1;
+                }
+            }
+        }
+    }
+
+    private static void output() {
+        for (int i = 1; i <= cntSubject; i++) {
+            System.out.print(result[i] + " ");
+        }
+    }
+
+    private static void input() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        cntSubject = Integer.parseInt(st.nextToken());
+        int cntEdge = Integer.parseInt(st.nextToken());
+
+        degree = new int[cntSubject + 1];
+        graph = new ArrayList<>();
+        for (int i = 0; i <= cntSubject; i++) {
+            graph.add(new ArrayList<>());
+        }
+
+        for (int i = 0; i < cntEdge; i++) {
+            st = new StringTokenizer(br.readLine());
+            int first = Integer.parseInt(st.nextToken());
+            int second = Integer.parseInt(st.nextToken());
+
+            graph.get(first).add(second);
+            degree[second]++;
+        }
+    }
+}
