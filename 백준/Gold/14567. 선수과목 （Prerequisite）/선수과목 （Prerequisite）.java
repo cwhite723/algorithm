6a59b937984db8133c4ba837a8633dd4a701de99
Hayan <33473174+cwhite723@users.noreply.github.com>
2023-06-15 00:32:39 +0900

    [Gold V] Title: 선수과목 (Prerequisite), Time: 688 ms, Memory: 128696 KB -BaekjoonHub

--- 백준/Gold/14567. 선수과목 （Prerequisite）/선수과목 （Prerequisite）.java
+++ 백준/Gold/14567. 선수과목 （Prerequisite）/선수과목 （Prerequisite）.java
@@ -1,6 +1,4 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.util.*;
 
 public class Main {
@@ -39,10 +37,13 @@ public class Main {
         }
     }
 
-    private static void output() {
+    private static void output() throws IOException {
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         for (int i = 1; i <= cntSubject; i++) {
-            System.out.print(result[i] + " ");
+            bw.write(result[i] + " ");
         }
+        bw.flush();
+        bw.close();
     }
 
     private static void input() throws IOException {
