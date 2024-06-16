ef386d5c477240276452993f30069dc474d283a4
Hayan <33473174+cwhite723@users.noreply.github.com>
2024-06-17 04:19:53 +0900

    [Silver III] Title: 풍선 터뜨리기, Time: 152 ms, Memory: 15672 KB -BaekjoonHub

--- 백준/Silver/2346. 풍선 터뜨리기/풍선 터뜨리기.java
+++ 백준/Silver/2346. 풍선 터뜨리기/풍선 터뜨리기.java
@@ -4,41 +4,46 @@ import java.io.InputStreamReader;
 import java.util.*;
 
 public class Main {
-    static class Node {
-        int index;
-        int value;
-
-        public Node(int index, int value) {
-            this.index = index;
-            this.value = value;
-        }
-    }
     public static void main(String[] args) throws IOException {
-        StringTokenizer st;
-        StringBuilder sb = new StringBuilder();
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int n = Integer.parseInt(br.readLine());
+        int totalBalloon = Integer.parseInt(br.readLine());
 
-        Deque<Node> list = new ArrayDeque<>();
-        st = new StringTokenizer(br.readLine());
-        for (int i = 0; i < n; i++) {
-            list.add(new Node(i+1, Integer.parseInt(st.nextToken())));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        Deque<Balloon> balloons = new ArrayDeque<>();
+        int index = 1;
+        while (st.hasMoreTokens()) {
+            balloons.offer(new Balloon(index++, Integer.parseInt(st.nextToken())));
         }
 
-        while (list.size() > 1) {
-            Node cur = list.pollFirst();
-            sb.append(cur.index).append(" ");
-            if (cur.value > 0) {
-                for (int i = 0; i < cur.value - 1; i++) {
-                    list.addLast(list.pollFirst());
+        StringBuilder answer = new StringBuilder();
+        Balloon current = balloons.pollFirst();
+        answer.append(current.index).append(" ");
+        while (balloons.size() > 1) {
+            int move = current.number;
+            if (move > 0) {
+                for (int i = 1; i < move; i++) {
+                    balloons.offerLast(balloons.pollFirst());
                 }
+                current = balloons.pollFirst();
             } else {
-                for (int i = 0; i < Math.abs(cur.value); i++) {
-                    list.addFirst(list.pollLast());
+                for (int i = -1; i > move; i--) {
+                    balloons.offerFirst(balloons.pollLast());
                 }
+                current = balloons.pollLast();
             }
+            answer.append(current.index).append(" ");
         }
-        sb.append(list.poll().index);
-        System.out.println(sb.toString());
+        answer.append(balloons.poll().index);
+        System.out.println(answer.toString());
+    }
+}
+
+class Balloon {
+    int index;
+    int number;
+
+    public Balloon(int index, int number) {
+        this.index = index;
+        this.number = number;
     }
 }
