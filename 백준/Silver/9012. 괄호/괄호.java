00a3dc9ecf6fb2696c5de056ec7ba04a6aa75189
Hayan <33473174+cwhite723@users.noreply.github.com>
2024-06-28 20:23:01 +0900

    [Silver IV] Title: 괄호, Time: 132 ms, Memory: 14308 KB -BaekjoonHub

--- 백준/Silver/9012. 괄호/괄호.java
+++ 백준/Silver/9012. 괄호/괄호.java
@@ -1,31 +1,39 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.*;
+import java.util.Stack;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         StringBuilder sb = new StringBuilder();
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int n = Integer.parseInt(br.readLine());
-        loop: for (int i=0; i<n; i++) {
-            String s = br.readLine();
-            Stack<Character> st = new Stack<>();
-            for (int k=0; k<s.length(); k++) {
-                Character c = s.charAt(k);
-                if(c=='(') st.push(c);
-                else if(c==')') {
-                    if(st.isEmpty()) {
-                        sb.append("NO\n");
-                        continue loop;
-                    } else {
-                        st.pop();
-                    }
-                }
-            }
-            if(!st.isEmpty()) sb.append("NO\n");
-            else sb.append("YES\n");
+        int number = Integer.parseInt(br.readLine());
+
+        for (int i = 0; i < number; i++) {
+            String str = br.readLine();
+            if (check(str)) sb.append("YES");
+            else sb.append("NO");
+
+            if (i != number - 1) sb.append("\n");
         }
-        System.out.println(sb.toString());
+
+        System.out.println(sb);
+    }
+
+    static boolean check(String str) {
+        Stack<Character> stack = new Stack<>();
+        for (int i = 0; i < str.length(); i++) {
+            char c = str.charAt(i);
+            if (c == '(') {
+                stack.push(c);
+            } else if (stack.isEmpty() || stack.peek() != '(') {
+                return false;
+            } else {
+                stack.pop();
+            }
+        }
+
+        if (!stack.isEmpty()) return false;
+        else return true;
     }
 }
